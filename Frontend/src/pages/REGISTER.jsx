52b83141710707ac41643540cb42eb008c6ea8e7
prathameshcodes03 <prathamesh.zone09@gmail.com>
2026-06-02 13:15:29 +0530

    Updated Ui for register page

--- Frontend/src/pages/REGISTER.jsx
+++ Frontend/src/pages/REGISTER.jsx
@@ -40,19 +40,20 @@ const REGISTER = () => {
     
 
             <div className='flex justify-center items-center w-screen h-screen bg-transparent ml-4'>
-                <div className='bg-[#080c28b9] w-[80%] sm:w-[55%] md:w-[45%] lg:w-[35%] flex flex-col justify-center gap-2 rounded-2xl items-center py-5'>
+                <div className='bg-[#080c28b9] w-[80%] sm:w-[60%] md:w-[40%] lg:w-[28%] flex flex-col justify-center gap-2 rounded-2xl items-center py-5 border border-purple-500/20 hover:border-purple-500/70 transition-colors duration-500'>
                     <div className='w-25 mx-3'>
                         <img src={logo} className='w-full' />
                     </div>
 
                     <div className='flex flex-col ml-2 '>
                         <div className='flex items-center justify-center gap-2 mx-auto'>
-                            <h1 className='text-3xl font-semibold text-center'>Create your account</h1>
-                            <Lollipop className='text-[#5e47b1]  hidden sm:block'/>
+                            <h1 className='text-3xl font-semibold mt-[-1rem]  text-center'>Create your account</h1>
+                           
                         </div>
                         
                         <div className='mx-auto px-2'>
-                            <p className='text-gray-300/70 text-lg mt-2 sm:mt-1 text-center'>join veil and get part of anonymous conversation</p>
+                            <p className='text-gray-300/70 text-lg mt-2 sm:mt-1 text-center'>Your identity stays hidden.
+Your voice doesn't.</p>
                         </div>
                         
                     </div>
@@ -61,26 +62,32 @@ const REGISTER = () => {
                         {error && <p className="text-red-500 text-sm">{error}</p>}
                         <div>
                             <h3 className='text-lg'>Display Name</h3>
-                            <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} className='border-2 border-[#5e47b1] rounded-2xl px-2 py-1 bg-transparent text-white' />
+                            <input type="text" name="Name" id="name" value={name} onChange={(e) => setName(e.target.value)}className='border-2 border-[#5e47b1] hover:border-purple-400 rounded-xl px-2 py-1 bg-transparent w-full sm:w-65 text-gray-500' />
                         </div>
 
                         <div>
                             <h3 className='text-lg'>Username</h3>
-                            <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} className='border-2 border-[#5e47b1] rounded-2xl px-2 py-1 bg-transparent text-white' />
+                            <input type="text" name="Username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} className='border-2 border-[#5e47b1] hover:border-purple-400 rounded-xl px-2 py-1 bg-transparent w-full sm:w-65 text-gray-500' />
                         </div>
 
                         <div>
-                            <h3 className='text-lg'>email</h3>
-                            <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className='border-2 border-[#5e47b1] rounded-2xl px-2 py-1 bg-transparent text-white' />
+                            <h3 className='text-lg'>Email</h3>
+                            <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className='border-2 border-[#5e47b1] hover:border-purple-400 rounded-xl px-2 py-1 bg-transparent w-full sm:w-65 text-gray-500' />
                         </div>
 
                         <div>
-                            <h3 className='text-lg'>password</h3>
-                            <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} minLength={6} className='border-2 border-[#5e47b1] rounded-2xl px-2 py-1 bg-transparent text-white' />
+                            <h3 className='text-lg'>Password</h3>
+                            <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} minLength={6}className='border-2 border-[#5e47b1] hover:border-purple-400 rounded-xl px-2 py-1 bg-transparent w-full sm:w-65 text-gray-500' />
                         </div>
 
-                        <input type="submit" value="Register" className='border-2 border-[#5e47b1] w-25 rounded-3xl mt-3 cursor-pointer px-1 py-2 hover:bg-[#5e47b1]'/>
-                        <p className="text-sm mt-2">Already have an account? <Link to="/login" className="text-[#5e47b1] hover:underline">Login</Link></p>
+                        <input type="submit" value="Register"  className="border border-purple-400/40 w-25 rounded-3xl mt-3 cursor-pointer px-4 py-2 text-white font-medium text-sm tracking-wide
+                            bg-white/5 backdrop-blur-sm
+                            hover:bg-purple-500/20 hover:border-purple-400/70 hover:shadow-[0_0_16px_rgba(168,85,247,0.25)]
+                            transition-all duration-300"/>
+
+
+
+                        <p className="text-sm mt-2">Already have an account? <Link to="/login" className="text-[#5e47b1] text-sm cursor-pointer font-semibold relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-px after:bg-purple-500 hover:after:w-full after:transition-all after:duration-300 hover:text-purple-500 transition-colors duration-300">Login</Link></p>
                     </form>
                 </div>
             </div>
